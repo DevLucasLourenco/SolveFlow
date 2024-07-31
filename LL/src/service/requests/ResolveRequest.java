693ec16118574a09306d54b1480faff104701401
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-07-31 08:00:03 -0300

    att

--- LL/src/service/requests/ResolveRequest.java
+++ LL/src/service/requests/ResolveRequest.java
@@ -1,5 +1,11 @@
 package service.requests;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+
+
 public class ResolveRequest {
     private Request request;
     private boolean concluded = false;
@@ -31,4 +37,25 @@ public class ResolveRequest {
         this.request = request;
     }
 
+}
+
+
+class EvalExpression {
+    public static void main(String[] args) {
+        // Cria uma instância do ScriptEngineManager
+        ScriptEngineManager manager = new ScriptEngineManager();
+        // Obtém uma instância do ScriptEngine para JavaScript
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+        String expression = "5 * 2";
+        
+        try {
+            // Avalia a expressão
+            Object result = engine.eval(expression);
+            // Imprime o resultado
+            System.out.println("Resultado: " + result);
+        } catch (ScriptException e) {
+            System.out.println(e);
+        }
+    }
 }
