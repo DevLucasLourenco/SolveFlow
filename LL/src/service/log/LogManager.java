dc5e56ae69efb9718be4c3afe511755b9f5e32d5
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-08-01 15:15:39 -0300

    att

---
+++ LL/src/service/log/LogManager.java
@@ -0,0 +1,26 @@
+package service.log;
+
+import java.util.logging.Logger;
+// import java.util.logging.Level;
+// import java.util.logging.ConsoleHandler;
+// import java.util.logging.FileHandler;
+// import java.util.logging.SimpleFormatter;
+
+
+public class LogManager {
+
+    private Logger LOGGER = Logger.getLogger(LogManager.class.getName());
+
+
+    private void prepareConsoleHandler(){
+        // ConsoleHandler(Level.ALL);
+    }
+
+    private void prepareFileHandler(){
+
+    }
+
+    private void configureLogLevel(){
+
+    }
+}
--- LL/src/service/log/logger.java
+++
@@ -1,5 +0,0 @@
-package service.log;
-
-public class logger {
-
-}
